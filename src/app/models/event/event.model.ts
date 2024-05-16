e9bbb854ba37f487f7486c6c4dc5ca98080be195
stetommy <99267639+stetommy@users.noreply.github.com>
2024-05-16 21:28:05 +0200

    feat: add events model

---
+++ src/app/models/event/event.model.ts
@@ -0,0 +1,77 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+enum Currency {
+  EUR = '€',
+  USD = '$',
+  CHF = 'CHF',
+  NZD = 'NZD',
+  GBP = 'GBP',
+  CAD = 'CAD',
+  AUD = 'AUD',
+  BTC = 'BTC',
+  ETH = 'ETH',
+}
+
+enum Type {
+  OP = 'Open Bar',
+  FE = 'Free Entry',
+  AC = 'A Consumazione',
+  PV = 'Privato',
+}
+
+export interface IEvent extends Document {
+  _id: string;
+  title: string;
+  sbtitle: string;
+  address: string;
+  special_guest: [ISpecialGuest];
+  tags: [];
+  description: string;
+  time_start: Date;
+  time_end: Date;
+  type: Type;
+  currency: Currency;
+  picture_id: string;
+  updated_on: Date;
+  created_by: Date;
+  details: JSON;
+}
+
+export interface ISpecialGuest extends Document {
+  _id: string;
+  name: string;
+}
+
+const specialGuestSchema: Schema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const eventSchema: Schema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    special_guest: {
+      type: specialGuestSchema,
+    },
+  },
+  { timestamps: true },
+);
+
+const eventModel = mongoose.model<IEvent>('Events', eventSchema);
+export default eventModel;
